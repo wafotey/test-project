b9ca21977628c23670b672d5ac4aa80a3e05d364
Williams <wafotey@outlook.com>
2021-08-02 12:25:59 +0000

    generated dist file

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/test-project'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/test-project/src/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/test-project/index.html'));
 })
 app.listen(process.env.PORT || 8080);
