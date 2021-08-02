9bcc64f19b5613eeffd90e02d734350eab39649a
Williams <wafotey@outlook.com>
2021-08-02 12:46:27 +0000

    dist folder to app/src

--- src/server.js
+++ src/server.js
@@ -6,3 +6,4 @@ app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname + '/dist/test-project/index.html'));
 });
 app.listen(process.env.PORT || 8080);
+console.log("Build started ...")
