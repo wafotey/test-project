6d47aa105f9dbd71a268e80632c8b373e2572363
Williams <wafotey@outlook.com>
2021-08-02 10:07:19 +0000

    Change port

--- src/server.js
+++ src/server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/test-project'));
 app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname + '/dist/test-project/index.html'));
 })
-app.listen(process.env.PORT || 800);
+app.listen(process.env.PORT || 8080);
